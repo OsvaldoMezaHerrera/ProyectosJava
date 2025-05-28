e6a9fc44fd3759a2c7a4c99149861752c4eafdfc
Osvaldo_Enrique_Meza_Herrera <20243ds051@utez.edu.mx>
2025-05-27 21:27:29 -0600

    cambios realizados en el codigo del prommedio

--- U1_TI_05_PromedioEstudiantes/Main.java
+++ U1_TI_05_PromedioEstudiantes/Main.java
@@ -2,58 +2,75 @@ import Model.Estudiante;
 
 public class Main {
     public static void main(String[] args) {
-        Estudiante[] estudiantes = new Estudiante[5];
-        
-        estudiantes[0] = new Estudiante();
-        estudiantes[0].setNombre("Brayan");
-        estudiantes[0].setMatricula("2025001");
-        estudiantes[0].setNota(0, 80);
-        estudiantes[0].setNota(1, 75);
-        estudiantes[0].setNota(2, 85);
-        estudiantes[0].setNota(3, 90);
-        estudiantes[0].setNota(4, 70);
-        
-        estudiantes[1] = new Estudiante();
-        estudiantes[1].setNombre("Alan");
-        estudiantes[1].setMatricula("2025002");
-        estudiantes[1].setNota(0, 70);
-        estudiantes[1].setNota(1, 70);
-        estudiantes[1].setNota(2, 78);
-        estudiantes[1].setNota(3, 76);
-        estudiantes[1].setNota(4, 80);
-        
-        estudiantes[2] = new Estudiante();
-        estudiantes[2].setNombre("Osvaldo");
-        estudiantes[2].setMatricula("2025003");
-        estudiantes[2].setNota(0, 100);
-        estudiantes[2].setNota(1, 100);
-        estudiantes[2].setNota(2, 100);
-        estudiantes[2].setNota(3, 100);
-        estudiantes[2].setNota(4, 100);
+        Estudiante estudiante1 = new Estudiante();
+        Estudiante estudiante2 = new Estudiante();
+        Estudiante estudiante3 = new Estudiante();
+        Estudiante estudiante4 = new Estudiante();
+        Estudiante estudiante5 = new Estudiante();
 
-        estudiantes[3] = new Estudiante();
-        estudiantes[3].setNombre("Nicho");
-        estudiantes[3].setMatricula("2025004");
-        estudiantes[3].setNota(0, 80);
-        estudiantes[3].setNota(1, 90);
-        estudiantes[3].setNota(2, 60);
-        estudiantes[3].setNota(3, 75);
-        estudiantes[3].setNota(4, 85);
+        estudiante1.setNombre("Brayan");
+        estudiante1.setMatricula("2025001");
+        estudiante1.setNota(0, 80);
+        estudiante1.setNota(1, 75);
+        estudiante1.setNota(2, 85);
+        estudiante1.setNota(3, 90);
+        estudiante1.setNota(4, 70);
+        
+        estudiante2.setNombre("Alan");
+        estudiante2.setMatricula("2025002");
+        estudiante2.setNota(0, 70);
+        estudiante2.setNota(1, 70);
+        estudiante2.setNota(2, 78);
+        estudiante2.setNota(3, 76);
+        estudiante2.setNota(4, 80);
+        
+        estudiante3.setNombre("Osvaldo");
+        estudiante3.setMatricula("2025003");
+        estudiante3.setNota(0, 100);
+        estudiante3.setNota(1, 100);
+        estudiante3.setNota(2, 100);
+        estudiante3.setNota(3, 100);
+        estudiante3.setNota(4, 100);
 
-        estudiantes[4] = new Estudiante();
-        estudiantes[4].setNombre("Jared");
-        estudiantes[4].setMatricula("2025005");
-        estudiantes[4].setNota(0, 65);
-        estudiantes[4].setNota(1, 75);
-        estudiantes[4].setNota(2, 68);
-        estudiantes[4].setNota(3, 73);
-        estudiantes[4].setNota(4, 55);
+        estudiante4.setNombre("Nicho");
+        estudiante4.setMatricula("2025004");
+        estudiante4.setNota(0, 80);
+        estudiante4.setNota(1, 90);
+        estudiante4.setNota(2, 60);
+        estudiante4.setNota(3, 75);
+        estudiante4.setNota(4, 85);
 
-        for(int i = 0; i < estudiantes.length; i++) {
-            System.out.println("Estudiante " + (i + 1) + ": " + estudiantes[i].getNombre());
-            System.out.println("Matricula: " + estudiantes[i].getMatricula());
-            System.out.println("Promedio: " + estudiantes[i].calcularPromedio());
-            System.out.println("Aprobado: " + estudiantes[i].aprobado());
-        }
+        estudiante5.setNombre("Jared");
+        estudiante5.setMatricula("2025005");
+        estudiante5.setNota(0, 65);
+        estudiante5.setNota(1, 75);
+        estudiante5.setNota(2, 68);
+        estudiante5.setNota(3, 73);
+        estudiante5.setNota(4, 55);
+
+        System.out.println("Estudiante 1: " + estudiante1.getNombre());
+        System.out.println("Matrícula: " + estudiante1.getMatricula());
+        System.out.println("Promedio: " + estudiante1.calcularPromedio());
+        System.out.println("Aprobado: " + estudiante1.aprobado());
+
+        System.out.println("Estudiante 2: " + estudiante2.getNombre());
+        System.out.println("Matrícula: " + estudiante2.getMatricula());
+        System.out.println("Promedio: " + estudiante2.calcularPromedio());
+        System.out.println("Aprobado: " + estudiante2.aprobado());
+
+        System.out.println("Estudiante 3: " + estudiante3.getNombre());
+        System.out.println("Matrícula: " + estudiante3.getMatricula());
+        System.out.println("Promedio: " + estudiante3.calcularPromedio());
+        System.out.println("Aprobado: " + estudiante3.aprobado());
+
+        System.out.println("Estudiante 4: " + estudiante4.getNombre());
+        System.out.println("Matrícula: " + estudiante4.getMatricula());
+        System.out.println("Promedio: " + estudiante4.calcularPromedio());
+        System.out.println("Aprobado: " + estudiante4.aprobado() );
+
+        System.out.println("Estudiante 5: " + estudiante5.getNombre());
+        System.out.println("Matrícula: " + estudiante5.getMatricula());
+        System.out.println("Promedio: " + estudiante5.calcularPromedio());
+        System.out.println("Aprobado: " + estudiante5.aprobado());
     }
 }
